5eadec57029091d54c0d3f0639651349bc726625
이수종 <137382468+issuejong@users.noreply.github.com>
2024-09-04 23:30:17 +0900

    Add files via upload

--- main/java/hello/core/member/MemberServiceImpl.java
+++ main/java/hello/core/member/MemberServiceImpl.java
@@ -3,9 +3,13 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     //회원을 찾기 위해서는 멤버 리포지토리 인터페이스가 필요하다
-    //인터페이스만 갖고 있으면 null 오류가 생기므로 new룰 해주는 것
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //인터페이스만 갖고 있으면 null 오류가 생기므로 new를 해주는 것
+    private final MemberRepository memberRepository;
 
+    //생성자를 통해 memberRepository에 구현체가 무엇이 들어갈지 결정함
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- test/java/hello/core/member/MemberServiceTest.java
+++ test/java/hello/core/member/MemberServiceTest.java
@@ -1,15 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void join() {
-
-        //임플로 불러오기
-        MemberService memberService = new MemberServiceImpl();
 
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
